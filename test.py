1ab7073f476b18155ced46a7484e67c599c97066
Diego Michel Prestes <diegoprestes99@hotmail.com>
2023-09-27 23:33:13 -0300

    remove test

--- test.py
+++
@@ -1,5 +0,0 @@
-import pyautogui as auto
-
-def main():
-    auto.locateOnScreen('./assets/tibia.png', grayscale=False, confidence=0.95)
-    # cords = auto.locateCenterOnScreen('./assets/' + img, grayscale=False, confidence=0.95)
